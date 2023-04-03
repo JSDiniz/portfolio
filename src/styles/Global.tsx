97b1bd0ad1782eb09d94c931f138471e64790859
JunielsonDiniz <juniosantos_@hotmail.com>
2023-04-03 11:39:03 -0300

    feat: modified package.json, styles/Global.tsx, utils/stackData.ts, utils/userData.ts and yarn.lock

--- src/styles/Global.tsx
+++ src/styles/Global.tsx
@@ -81,11 +81,10 @@ export const globalStyles = globalCss({
 });
 
 export const GlobalStyle = ({ children }: GlobalStyleProps): JSX.Element => {
-
   const theme = createTheme({
     colors: {
-      brand1: "#623CEA",
-      brand2: "#311E75",
+      brand1: "#30b387",
+      brand2: "#38d39f",
 
       grey0: "#0A0A0B",
       grey1: "#121214",
--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -1,9 +1,9 @@
 export const userData = {
-  nameUser: "Samuel Leão",
-  githubUser: "samuelleaokenzie",
-  linkedinUser: "nekelpatrick",
-  whatsappNumber: "+5541988819028",
-  emailUser: "samuel@kenzie.com.br",
-  instagramUser: "samuelleaoui",
+  nameUser: "Junielson Diniz",
+  githubUser: "JSDiniz",
+  linkedinUser: "junielson-diniz",
+  whatsappNumber: "",
+  emailUser: "juniosantos_@hotmail.com",
+  instagramUser: "",
   facebookUser: "",
 };
